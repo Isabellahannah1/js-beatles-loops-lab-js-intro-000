1d88bac28bbb2488301b0068abced0659657cdcf
Isabella Hannah <isabellahannah735@gmail.com>
2019-05-21 15:43:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
-
-function theBeatlesPlay(musicians, instruments){
-return musicians;
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+  for (var i = 0; i < musicians.length; i++) {
+  	array.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return array;
 }
-
-var empty = [];
