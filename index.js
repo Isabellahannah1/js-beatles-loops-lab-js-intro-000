64e8c10d11a76e73149df1f9b43097a93c85f4ee
Isabella Hannah <isabellahannah735@gmail.com>
2019-05-21 15:39:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
-// add solution here
-function theBeatlesPlay(musicians, instruments) {
-  var array = [];
-  for (var i = 0; i < musicians.length; i++) {
-  	array.push(musicians[i] + ' plays ' + instruments[i]);
-  }
-  return array;
+var musicians = 
+function theBeatlesPlay(musicians, instruments){
+return musicians;
 }
 
-function johnLennonFacts(facts) {
-  var results = [];
-  var count = 0;
-  while (count < facts.length) {
-    results.push(facts[count] + "!!!");
-    count++;
-  }
-  return results;
-}
-
-function iLoveTheBeatles(num) {
-  var array = [];
-  do {
-    array.push("I love the Beatles!")
-    num++;
-  } while (num < 15);
-  return array;
-}
+var empty = [];
