cace2f1b074ebbe026f1ddde9bd5cfab49c7a21f
Isabella Hannah <isabellahannah735@gmail.com>
2019-05-21 15:41:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var musicians = 
+var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
 function theBeatlesPlay(musicians, instruments){
 return musicians;
 }
